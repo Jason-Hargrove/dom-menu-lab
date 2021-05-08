653265825b2387e9e1867bdfac76db885f63e628
Jason Hargrove <rijaha@gmail.com>
2021-05-08 13:36:38 -0500

    Round one

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// let mainEl = document.querySelector('main');
+// const h1 = document.createElement('h1');
+// const mainTitle = h1.innerHTML = 'SEI Rocks!',
+//
+// // mainEl.style.backgroundColor = '#4a4e4d';
+//
+// mainEl.style.backgroundColor = 'var(--main-bg)';
+//
+// mainEl.setAttribute('class', 'main-thing');
+//
+// mainEl.appendChild(mainTitle);
+//
+// console.log(mainEl);
+
+
+// Tasks 1.0
+
+const mainEl = document.querySelector('main')
+
+// Task 1.1
+
+mainEl.style.backgroundColor = 'var(--main-bg)'
+
+// Task 1.2
+
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
+
+// Task 1.3
+
+mainEl.classList.add('flex-ctr');
